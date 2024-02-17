b8a654ce5f8d1a2bb4c579fc848f2e6ddd472dbf
RupiniRavichandran <37034723+RupiniRavichandran@users.noreply.github.com>
2024-02-17 22:59:22 +0530

    Update streamlit_app.py
    BreakFast Favorites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebeery Oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Bioled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Bluebeery Oatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Bioled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
